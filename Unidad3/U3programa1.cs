5a312439ae8abf268d301160a4c19b40455eebe8
Emilianocelaya <146400735+Emilianocelaya@users.noreply.github.com>
2023-10-18 23:12:52 -0500

    Add files via upload

---
+++ Unidad3/U3programa1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace U3programa1 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+
+                int e;
+
+                Console.WriteLine("Ingresa tu edad:");
+                e = Convert.ToInt32(Console.ReadLine());
+
+                if (e >= 18)
+                {
+                    Console.WriteLine("Eres Mayor");
+                }
+                else
+                {
+                    Console.WriteLine("Eres Menor");
+                }
+            }
+        }
+    }
+}
