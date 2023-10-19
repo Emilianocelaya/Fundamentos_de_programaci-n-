84faa67e149f8b5ed3dd9106f60ce17fd47b8020
Emilianocelaya <146400735+Emilianocelaya@users.noreply.github.com>
2023-10-18 20:49:09 -0500

    Add files via upload

--- Unidad3/Program.cs
+++ Unidad3/Program.cs
@@ -1,41 +1,54 @@
 ﻿using System;
 
-namespace U3programa3 // Note: actual namespace depends on the project name.
+namespace U3programa1 // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int dia;
-            Console.WriteLine ("Programa para los dias de la semana");
-            Console.WriteLine("introdusca un numero para saber que dia es: ");
-            dia = Convert.ToInt32 (Console.ReadLine());
-            switch (dia) 
+            int mes;
+           Console.WriteLine("Programa para los MESES DEL AÑO");
+            Console.WriteLine("introdusca un numero para saber que MES es: ");
+            mes = Convert.ToInt32(Console.ReadLine());
+            switch (mes)
             {
                 case 1:
-                    Console.WriteLine("Es lunes");
+                    Console.WriteLine("Es Enero");
                     break;
                 case 2:
-                    Console.WriteLine("Es Martes");
+                    Console.WriteLine("Es Febrero");
                     break;
                 case 3:
-                    Console.WriteLine("Es Miercoles");
+                    Console.WriteLine("Es Marzo");
                     break;
                 case 4:
-                    Console.WriteLine("Es Jueves");
+                    Console.WriteLine("Es Abril");
                     break;
                 case 5:
-                    Console.WriteLine("Es Viernes ");
+                    Console.WriteLine("Es Mayo ");
                     break;
                 case 6:
-                    Console.WriteLine("Es Sabado ");
+                    Console.WriteLine("Es Junio ");
                     break;
                 case 7:
-                    Console.WriteLine("Es Domingo ");
+                    Console.WriteLine("Es Julio ");
                     break;
-                default:
-                    Console.WriteLine("no entra del 1 al 7 ");
+                case 8:
+                    Console.WriteLine("Es Agosto ");
                     break;
+                case 9:
+                    Console.WriteLine("Es Septiembre ");
+                    break;
+                case 10:
+                    Console.WriteLine("Es Octubre  ");
+                    break;
+                case 11:
+                    Console.WriteLine("Es Noviembre ");
+                    break;
+                case 12:
+                    Console.WriteLine("Es Diciembre ");
+                    break;
+
             }
 
         }
@@ -43,4 +56,3 @@ namespace U3programa3 // Note: actual namespace depends on the project name.
 
 }
     
-
