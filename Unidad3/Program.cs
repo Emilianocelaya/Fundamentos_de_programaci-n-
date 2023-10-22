5ac9b9d95a0fdff80f2761db32c61d29df8a1941
Emilianocelaya <146400735+Emilianocelaya@users.noreply.github.com>
2023-10-22 14:44:02 -0500

    Add files via upload

---
+++ Unidad3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace U3programa6 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        
+
+            {
+               
+                int f, c;
+                Console.WriteLine("declarar el valor de los grados c: ");
+                c = Convert.ToInt32(Console.ReadLine());
+                f = (c * 9 / 5) + 32;
+                Console.WriteLine(" equivale a los grados fharenhei: " + f);
+            }
+        }
+    }
+}
+
