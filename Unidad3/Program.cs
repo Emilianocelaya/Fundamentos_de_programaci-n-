c776967e73dafae069e4772d9ff61c0dd73e6ef1
Emilianocelaya <146400735+Emilianocelaya@users.noreply.github.com>
2023-10-22 15:19:36 -0500

    Add files via upload

---
+++ Unidad3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace U3programa8// Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double p1, p2, p3, subt, t, ivaT;
+             double iva = 0.16;
+
+            Console.WriteLine("¿Qué precio tiene el 1er producto? ");
+            p1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("¿Qué precio tiene el 2do producto? ");
+            p2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("¿Qué precio tiene el 3er producto? ");
+            p3 = Convert.ToInt32(Console.ReadLine());
+
+            t = p1 + p2 + 3;
+            ivaT = t * iva;
+            subt = t - ivaT;
+            Console.WriteLine("El toal es: " + t);
+            Console.WriteLine("El subtotal es: " + subt);
+            Console.WriteLine("El IVA es: " + ivaT);
+        }
+    }
+}
+    
+
