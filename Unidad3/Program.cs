4b5a0d98656b62c93e3e6d2d4563c00cddde5a34
Emilianocelaya <146400735+Emilianocelaya@users.noreply.github.com>
2023-10-23 20:45:22 -0500

    Add files via upload

---
+++ Unidad3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace U3programa9 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double hr, suma = 0;
+           Console.WriteLine("escribe las horas : ");
+            hr = Convert.ToInt32(Console.ReadLine());
+            if (hr > 1.01 && hr <= 2.00)
+                suma = suma + 5;
+            else if (hr > 1.01 && hr <= 2.00)
+                suma = suma + 10;
+            Console.WriteLine("total a apgar $ +suma : ");
+        }
+    }
+}
