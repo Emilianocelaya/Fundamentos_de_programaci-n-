f2fbb1cc359e20d0634f144a32e82eaea9023327
Emilianocelaya <146400735+Emilianocelaya@users.noreply.github.com>
2023-10-27 18:35:20 -0500

    Add files via upload

---
+++ Unidad3/U3programa16.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.u3programa16;
+
+/**
+ *
+ * @author LaboratorioLTI2
+ */
+public class U3programa16 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
