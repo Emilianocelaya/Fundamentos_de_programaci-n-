3468db2c18d8390fc42202ccfe39acbe4eaba11e
Emilianocelaya <146400735+Emilianocelaya@users.noreply.github.com>
2023-10-29 15:21:06 -0500

    Add files via upload

--- Unidad3/U3programa15.java
+++ Unidad3/U3programa15.java
@@ -3,18 +3,27 @@
  */
 
 package com.mycompany.u3programa15;
-
+    import java.util.Scanner;
 /**
  *
- * @author LaboratorioLTI2
+ * @author Emiliano
  */
 public class U3programa15 {
 
     public static void main(String[] args) {
-       int c=1;
-       while (c<=20){
-           System.out.println (c);
-           c++;
-       }
+    Scanner sc = new Scanner(System.in);
+    int i=1, horas,pago,sueldo ;
+    while(i<=20)
+    {
+    System.out.println("ingrese las horas trabajadas en la semana: ");  
+    horas = sc.nextInt();
+    System.out.println("ingrese el pago por hora  ");  
+    pago = sc.nextInt();      
+     sueldo = (pago*horas ) ;
+        System.out.println("El pago es : " + sueldo);
+        i++;
     }
+}
+    
+
 }
