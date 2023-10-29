becacec3f0d339e1467fcf00d8c7417f55cb1de8
Emilianocelaya <146400735+Emilianocelaya@users.noreply.github.com>
2023-10-29 16:06:01 -0500

    Add files via upload

---
+++ Unidad3/U3programa15e.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.u3programa15e;
+
+/**
+ *
+ * @author Emiliano
+ */
+public class U3programa15e {
+
+    public static void main(String[] args) {
+       int c = 1, suma = 0;
+       while(c<=20){
+           suma=suma+c;
+        System.out.println("suma=" + suma);      
+        c++;
+       }
+    }
+}
