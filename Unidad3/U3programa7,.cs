f90fba6cdba51b24f2187386f123ba5245d4da04
Emilianocelaya <146400735+Emilianocelaya@users.noreply.github.com>
2023-10-30 20:01:18 -0600

    Add files via upload

---
+++ Unidad3/U3programa7,.cs
@@ -0,0 +1,26 @@
+﻿namespace U3programa7 // Note: actual namespace depends on the project name.
+{
+    internal class Temperatura
+    {
+        static void Main(string[] args)
+        {
+            double T;
+            Console.WriteLine("¿Qué temperatura hay hoy?");
+            T = double.Parse(Console.ReadLine());
+
+            if (T >= 27)
+            {
+                Console.WriteLine("Hace calor");
+            }
+            else if (T < 27 && T >= 20)
+            {
+                Console.WriteLine("El clima hoy es agradable");
+            }
+            else
+            {
+                Console.WriteLine("Hace frío");
+
+            }
+        }
+    }
+}
