26e8bb1231cff7135b912dd9857b12713b77bfbc
Emilianocelaya <146400735+Emilianocelaya@users.noreply.github.com>
2023-10-30 20:23:17 -0600

    Update U3programa9.java

--- Unidad3/U3programa9.java
+++ Unidad3/U3programa9.java
@@ -6,19 +6,45 @@ package com.mycompany.u3programa9;
 import java.util.Scanner;
 /**
  *
- * @author LaboratorioLTI2
+ * @author DELL
  */
 public class U3programa9 {
 
     public static void main(String[] args) {
-    Scanner sc= new Scanner (System.in);
-     double hr,suma=0 ;
-       System.out.print ("escribe las horas : ");
-       hr=sc.nextDouble();
-       if(hr>1.01 && hr<=2.00)
-           suma=suma+5;
-       else if(hr>1.01 && hr<=2.00)
-           suma=suma+10;
-        System.out.print ("total a apgar $ +suma : ");
+        Scanner sc= new Scanner(System.in);
+            int h1, h2, h3;
+            double costo = 0;
+            System.out.println("Utiliza el formato de 24 horas.");
+            System.out.println("¿Cual fue la hora de entrada? ");
+            h1= sc.nextInt();
+            System.out.println("¿Cual fue la hora de salida? ");
+            h2 = sc.nextInt();
+
+            h3 = h2 - h1;
+            System.out.println("Usted estuvo " + h3 + " horas en total");
+
+            if (h3 <= 2)
+            {
+                costo = h3 * 5;
+                System.out.println("Por las " + h3 + " horas que estuvo aqui, tiene que pagar $" + costo);
+            }
+
+            else if (h3 <= 5)
+            {
+                costo = 2 * 5 + (h3 - 2) * 4;
+                System.out.println("Por las " + h3 + " horas que estuvo aqui, tiene que pagar $" + costo);
+            }
+
+            else if (h3 <= 10)
+            {
+                costo = 2 * 5 + 3 * 4 + (h3 - 5) * 3;
+                System.out.println("Por las " + h3 + " horas que estuvo aqui, tiene que pagar $" + costo);
+            }
+
+            else
+            {
+                costo = 2 * 5 + 3 * 4 + 5 * 3 + (h3 - 10) * 2;
+                System.out.println("Por las " + h3 + " horas que estuvo aqui, tiene que pagar $" + costo);
+            }
     }
 }
