7b63e24771660a96c61882f531378d83ee84acac
Emilianocelaya <146400735+Emilianocelaya@users.noreply.github.com>
2023-10-30 20:21:16 -0600

    Add files via upload

---
+++ Unidad3/U3programa6.java
@@ -0,0 +1,20 @@
+
+package com.mycompany.u3programa6;
+import java.util.Scanner;
+/**
+ *
+ * @author Emiliano
+ */
+public class U3programa6 {
+ public static void main(String[] args) {
+ Scanner sc= new Scanner(System.in);
+ int n ;
+ System.out.println("Ingrese el un número :");
+ n =sc.nextInt();
+ if (n % 2 == 0)
+ System.out.println(" es par");
+ else
+ System.out.println("es  impar");
+
+    }
+}
