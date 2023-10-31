d7844d7d7dc9e91d88ed5298a236155d1195609f
Emilianocelaya <146400735+Emilianocelaya@users.noreply.github.com>
2023-10-30 20:48:59 -0600

    Add files via upload

---
+++ Unidad3/U3programa16.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace U3programa16 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1, h, p, s;
+            while (i <= 20)
+            {
+                Console.WriteLine("Ingrese la cantidad de horas trabajadas en la semana: ");
+                h = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ingrese el pago por horas: ");
+                p = Convert.ToInt32(Console.ReadLine());
+                s = h * p;
+                Console.WriteLine("el pago es es: " + s);
+                i++;
+            }
+        }
+    }
+}
---
+++ Unidad3/U3programa188.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.u3programa188;
+
+/**
+ *
+ * @author Emiliano
+ */
+public class U3programa188 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
