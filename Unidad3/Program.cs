9368e367b28955426b8e477deac06b31c1d621b6
Emilianocelaya <146400735+Emilianocelaya@users.noreply.github.com>
2023-10-30 19:47:49 -0600

    Add files via upload

--- Unidad3/Program.cs
+++ Unidad3/Program.cs
@@ -1,20 +1,31 @@
 ﻿using System;
 
-namespace U3programa10 // Note: actual namespace depends on the project name.
+namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int Lado, Perimetro, Apotema, area;
-            Console.WriteLine("declarar el valor de L ");
-            Lado  = Convert.ToInt32(Console.ReadLine());
-            Perimetro = Lado * 6;
-            Console.WriteLine("declarar el valor del apotema ");
-            Apotema = Convert.ToInt32(Console.ReadLine());
-            area = (Perimetro * Apotema) / 2;
-            Console.WriteLine("El ´área del hexagono es: " + area);
+            int hrtrab, hrext;
+            double pago, sueldosem = 0;
+            Console.WriteLine("¿Cuantas horas trabajaste?");
+            hrtrab = Convert.ToInt32(Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine("¿Cuanto es el pago por hora?");
+            pago = Convert.ToInt32(Convert.ToInt32(Console.ReadLine()));
+
+            if (hrtrab <= 40)
+            {
+                sueldosem = hrtrab * pago;
+                Console.WriteLine("Recibiras un total de $" + sueldosem);
+            }
+            else
+            {
+                hrext = hrtrab - 40;
+                sueldosem = 40 * pago + hrext * (pago * 2);
+                Console.WriteLine("Por trabajar " + hrext + " horas extra, usted recibe un total de $" + sueldosem);
+
+
+            }
         }
     }
 }
-    
