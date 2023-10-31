cf26bd33e4d34459d01b6d32e45fb1156b03fda3
Emilianocelaya <146400735+Emilianocelaya@users.noreply.github.com>
2023-10-30 20:33:14 -0600

    Add files via upload

---
+++ Unidad3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace U3programa12 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int c, total, num;
+            Console.WriteLine("escribe el total de los numeros: ");
+            total = Convert.ToInt32(Console.ReadLine());
+            for (c = 1; c <= total; c++)
+            {
+                Console.WriteLine("escribe numeros: ");
+                num = Convert.ToInt32(Console.ReadLine());
+                if (num % 2 == 0)
+                    Console.WriteLine("Par");
+                else
+                    Console.WriteLine("Impar");
+            }
+        }
+    }
+}
