65b3dfad800a4d594126aac867a794cab2cfbf37
Emilianocelaya <146400735+Emilianocelaya@users.noreply.github.com>
2023-10-30 19:21:39 -0600

    Update Proyecto6.java

--- Unidad3/Proyecto6.java
+++ Unidad3/Proyecto6.java
@@ -1,21 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
-package com.mycompany.proyecto6;
+package com.mycompany.u3programa6;
 import java.util.Scanner;
 /**
  *
- * @author LaboratorioLTI2
+ * @author Emiliano
  */
-public class Proyecto6 {
+public class U3programa6 {
+ public static void main(String[] args) {
+ Scanner sc= new Scanner(System.in);
+ int n ;
+ System.out.println("Ingrese el un número :");
+ n =sc.nextInt();
+ if (n % 2 == 0)
+ System.out.println(" es par");
+ else
+ System.out.println("es  impar");
 
-    public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        int f,c;
-        System.out.print(" ingresar los grados c: ");
-        c = sc.nextInt ();
-        f = (c * 9/5) + 32;
-        System.out.print(" ees igual a los grados fharenhei: " + f);
     }
 }
