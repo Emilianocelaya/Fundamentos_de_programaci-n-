0646a3e05fba7a0d8ca6dcfbbb8c08d76e0ca5bd
Emilianocelaya <146400735+Emilianocelaya@users.noreply.github.com>
2023-11-08 19:28:03 -0600

    Add files via upload

---
+++ UNIDAD4/Programa2u4.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.NetworkInformation;
+
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] cali = new double[10] {9, 8,  7, 7, 8, 8, 9, 8, 6, 7,};
+            double prom = 0, suma = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                suma = suma + cali[i];
+                prom = suma / 10;
+            }
+            Console.WriteLine("tu promedio es de: " + prom);
+        }
+    }
+}
