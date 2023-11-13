0063c2aadf299a4a1f986c6cc7a7181645cfbeef
Emilianocelaya <146400735+Emilianocelaya@users.noreply.github.com>
2023-11-12 23:41:56 -0600

    Add files via upload

---
+++ UNIDAD4/programa10u4.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] paises = { "Namekuzein", "España", "china", "australia", "japon", "inglaterra" };
+
+
+        double longitudPromedio = CalcularLongitudPromedio(paises);
+        Console.WriteLine("Longitud Promedio de las Palabras: " + longitudPromedio);
+    }
+
+    static double CalcularLongitudPromedio(string[] arreglo)
+    {
+        int totalCaracteres = 0;
+
+        foreach (string palabra in arreglo)
+        {
+            totalCaracteres += palabra.Length;
+        }
+
+        return (double)totalCaracteres / arreglo.Length;
+    }
+}
+
+
