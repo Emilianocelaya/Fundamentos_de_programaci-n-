53f4bad86a8aa807594c169573dd2e1251cb8d0c
Emilianocelaya <146400735+Emilianocelaya@users.noreply.github.com>
2023-11-12 23:30:11 -0600

    Add files via upload

---
+++ UNIDAD4/Programa8u4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numeros = { 4, 22, 31, 46, 57, 63, 72, 81, 92, 35 };
+
+        Console.WriteLine("Números Pares:");
+        foreach (int num in numeros)
+        {
+            if (EsPar(num))
+            {
+                Console.Write(num + " ");
+            }
+        }
+
+        Console.WriteLine("\nNúmeros Impares:");
+        foreach (int num in numeros)
+        {
+            if (!EsPar(num))
+            {
+                Console.Write(num + " ");
+            }
+        }
+    }
+
+    static bool EsPar(int numero)
+    {
+        return numero % 2 == 0;
+    }
+}
+
