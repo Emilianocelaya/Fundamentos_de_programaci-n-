c33dcbccbdd54b44e3ec7a42ce195d8bd866bb0d
Emilianocelaya <146400735+Emilianocelaya@users.noreply.github.com>
2023-11-21 19:44:45 -0600

    Add files via upload

---
+++ UNIDAD4/PROGRAMA14U4.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programa14u4;
+
+/**
+ *
+ * @author LaboratorioLTI2
+ */
+public class PROGRAMA14U4 {
+
+    public static void main(String[] args) {
+       int [] cali = new int[3];
+     cali [0]= 5;
+     cali [1]= 7;
+     cali [2]= 9;
+   
+     
+   System.out.println("los datos son");
+    for(int i=0; i<3; i++){
+  
+   System.out.println(cali[i]);
+    }
+    int suma;
+    suma = cali[0] + cali[1] + cali[2];
+     System.out.println("la suma de los datos es " + suma);
+    
+        
+        
+    }
+}
+   
+
