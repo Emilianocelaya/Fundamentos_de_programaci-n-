f3a417d86bc6611c89e66bd9724421b707256a69
Emilianocelaya <146400735+Emilianocelaya@users.noreply.github.com>
2023-11-26 22:45:37 -0600

    Add files via upload

--- UNIDAD4/Program.cs
+++ UNIDAD4/Program.cs
@@ -6,42 +6,66 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
+            Console.Write("Ingrese el número de filas: ");
+            int fila = int.Parse(Console.ReadLine());
 
-        }
-    }
+            Console.Write("Ingrese el número de columnas: ");
+            int columna = int.Parse(Console.ReadLine());
 
+            int[][] matrix = new int[fila][];
 
-
-
-
-    namespace U4programa15 // Note: actual namespace depends on the project name.
-    {
-        internal class Program
-        {
-            static void Main(string[] args)
+            for (int i = 0; i < fila; i++)
             {
-                int[] matriz = new int[3];
-                matriz[0] = 3;
-                matriz[1] = 5;
-                matriz[2] = 2;
-
-                Console.WriteLine("Contenido del arreglo");
-                for (int i = 0; i < 3; i++)
-                {
-                    Console.WriteLine(matriz[i]);
-
-                }
-                double media;
-                media = (matriz[0] + matriz[1] + matriz[2]) / 3;
-                Console.WriteLine("La media de los arreglos en la matriz es: " + media);
+                matrix[i] = new int[columna];
             }
+
+            Console.WriteLine("Ingrese los elementos de la matriz:");
+
+            for (int i = 0; i < fila; i++)
+            {
+                for (int j = 0; j < columna; j++)
+                {
+                    matrix[i][j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            int[] result = CountEvenOdd(matrix);
+
+            Console.WriteLine("La cantidad de números pares es: " + result[0]);
+            Console.WriteLine("La cantidad de números impares es: " + result[1]);
+        }
+
+        public static int[] CountEvenOdd(int[][] matrix)
+        {
+            int fila = matrix.Length;
+            int columna = matrix[0].Length;
+
+            int cpar = 0;
+            int cimpar = 0;
+
+            for (int i = 0; i < fila; i++)
+            {
+                for (int j = 0; j < columna; j++)
+                {
+                    if (matrix[i][j] % 2 == 0)
+                    {
+                        cpar++;
+                    }
+                    else
+                    {
+                        cimpar++;
+                    }
+                }
+            }
+
+            return new int[] { cpar, cimpar };
         }
     }
+
+
 }
-
-
-
-
-   
     
+
+
+
 
