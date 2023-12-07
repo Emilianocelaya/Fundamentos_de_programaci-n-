0af9dd411ad5177efb15c27c543bd5c67f037434
Emilianocelaya <146400735+Emilianocelaya@users.noreply.github.com>
2023-12-07 14:11:56 -0600

    Add files via upload

---
+++ unidad5/unidad5programa8.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace unidad5programa8 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Random ale = new Random();
+          
+            int x = ale.Next(1, 7);
+            Console.WriteLine("Numero de la cara del dado: " + x);
+
+        }
+    }
+}
