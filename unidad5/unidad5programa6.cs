e2c9736523697438c955432c7355d78b20fb7d7a
Emilianocelaya <146400735+Emilianocelaya@users.noreply.github.com>
2023-12-07 14:03:12 -0600

    Add files via upload

---
+++ unidad5/unidad5programa6.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace unidad5programa6 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            f();
+        }
+        public static void f()
+        {
+            int a = 0, b = 1, c = 0;
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.Write(a + " ");
+                c = a + b;
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
+    
