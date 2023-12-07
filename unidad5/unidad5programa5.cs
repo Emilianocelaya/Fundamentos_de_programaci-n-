fe324694a1b306e9afea91c1517279ec0b073bd6
Emilianocelaya <146400735+Emilianocelaya@users.noreply.github.com>
2023-12-07 13:59:55 -0600

    Add files via upload

---
+++ unidad5/unidad5programa5.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace unidad5programa5 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           string p = "";
+            Console.Write("escribe una palabra : ");
+            p = Console.ReadLine();
+            Console.WriteLine(p + " esta palabra esta conformada por : " + palabras(p) + " letras");
+        }
+        public static int palabras(string x)
+        {
+            int s = x.Length;
+            return s;
+        }
+    }
+}
+    
+
