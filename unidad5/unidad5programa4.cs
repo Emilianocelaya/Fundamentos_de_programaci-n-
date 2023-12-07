c43994b08b8200a9d0b32d0811e15c56e2acc84d
Emilianocelaya <146400735+Emilianocelaya@users.noreply.github.com>
2023-12-07 13:51:06 -0600

    Add files via upload

---
+++ unidad5/unidad5programa4.cs
@@ -0,0 +1,33 @@
+﻿namespace unidad5programa4;
+
+class Numero_P
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("escribe un numero: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(numero + " " + primo(numero));
+    }
+    public static string primo(int x)
+    {
+        int c = 0;
+        string p;
+        for (int i = x; i > 0; i--)
+        {
+            if (x % i == 0)
+            {
+                c++;
+            }
+        }
+        if (c == 2)
+        {
+            p = "es  primo";
+        }
+        else
+        {
+            p = "no es  primo";
+        }
+        return p;
+    }
+
+}
