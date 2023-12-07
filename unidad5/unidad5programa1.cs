66dc87d28b8182e53ea7247aa323e8a611242b47
Emilianocelaya <146400735+Emilianocelaya@users.noreply.github.com>
2023-12-07 13:39:45 -0600

    Add files via upload

---
+++ unidad5/unidad5programa1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace ProgramaU5_1;
+
+class Calculadora
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Suma: " + suma(7, 8));
+        Console.WriteLine("Resta: " + resta(10, 2));
+        Console.WriteLine("Multiplicacion: " + multi(3, 8));
+        Console.WriteLine("division: " + divicion(10, 2));
+    }
+    // SUMA
+    public static int suma(int a, int b)
+    {
+        int res = a + b;
+        return res;
+    }
+    // RESTA
+    public static int resta(int a, int b)
+    {
+        int res = a - b;
+        return res;
+    }
+    // MULTIPLICACION
+    public static int multi(int a, int b)
+    {
+        int res = a * b;
+        return res;
+    }
+    // DIVISION
+    public static int divicion(int a, int b)
+    {
+        int res = a / b;
+        return res;
+    }
+}
