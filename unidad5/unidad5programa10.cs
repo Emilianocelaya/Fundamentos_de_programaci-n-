72f16c04ac862533b6edbf3471ed344714933baa
Emilianocelaya <146400735+Emilianocelaya@users.noreply.github.com>
2023-12-07 14:17:30 -0600

    Add files via upload

---
+++ unidad5/unidad5programa10.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        int longitud = 8;
+        string contrasena = Aleatorio(longitud);
+        Console.WriteLine("password: " + contrasena);
+    }
+
+    public static string Aleatorio(int longitud)
+    {
+        string caracteres = "ABCDEFGHIJK123456789*-_.";
+        Random random = new Random();
+        StringBuilder cadena = new StringBuilder(longitud);
+        for (int i = 0; i < longitud; i++)
+        {
+            int indiceCaracter = random.Next(caracteres.Length);
+            cadena.Append(caracteres[indiceCaracter]);
+        }
+        return cadena.ToString();
+    }
+}
