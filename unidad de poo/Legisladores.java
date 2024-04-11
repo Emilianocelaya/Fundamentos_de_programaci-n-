cb5ecec7cbbe17c6ba748641a84fa6d6fcc750d5
Emilianocelaya <146400735+Emilianocelaya@users.noreply.github.com>
2024-04-11 09:42:39 -0600

    Add files via upload

---
+++ unidad de poo/Legisladores.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.legisladores;
+
+/**
+ *
+ * @author SALA PAFP 26
+ */
+public class Legisladores {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
