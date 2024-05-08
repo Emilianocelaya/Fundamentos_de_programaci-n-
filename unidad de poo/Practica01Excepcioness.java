5219839ecc7a0db8699fa971ca22bb5524de52d7
Emilianocelaya <146400735+Emilianocelaya@users.noreply.github.com>
2024-05-08 09:52:50 -0600

    Add files via upload

---
+++ unidad de poo/Practica01Excepcioness.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica01excepcioness;
+
+/**
+ *
+ * @author SALA PAFP 26
+ */
+public class Practica01Excepcioness {
+ public static void main(String[] args) {
+   clasePractica01Excepciones.primeraExcepcion();
+    clasePractica01Excepciones.combinandoExcepciones();
+}
+}
